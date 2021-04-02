01248a2bf02ea8d125695deaec44babd37f7cd49
olvanotpoint@gmail.com <0lvanotpoint@gmail.com>
2021-04-03 01:39:26 +0300

    data base config

--- database/migrations/2020_12_15_204728_create_exam_table.php
+++ database/migrations/2020_12_15_204728_create_exam_table.php
@@ -22,8 +22,8 @@ class CreateExamTable extends Migration
             $table->dateTime("end_time");
             $table->time("delay");
             $table->string("result");
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->nullable(now());
+            $table->timestamp('updated_at')->nullable(now());
         });
     }
 
